92887a952fe8d186d27a30744da3370af304bc3c
zjkl19 <eamdfan@126.com>
2019-03-21 11:43:52 +0800

    add initial result

--- SuspensionBridgeCableLength/HTCableLength.py
+++ SuspensionBridgeCableLength/HTCableLength.py
@@ -15,13 +15,26 @@ import logging
 logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+
 h=80.107-23.732    #y|x=l 处高度
 f=15.114    #矢高
 l=150    #跨径
 n=f/l  #矢跨比
 
-x = sympy.symbols("x")
+Ec=2.0*10**11   #主缆弹性模量(国际单位）
+Ac=0.25*3.14*0.35644**2   #主缆面积（国际单位）
+gb=182734.2   #加劲梁自重（P58 有解释）。计算过程详见excel表格（联合midas计算）
+H=gb*l**2/(8*f)    #主缆拉力的水平分力（P57 3-2-10）
+gc=78.2*10**3*Ac   #缆索沿纵桥向每延米自重
 
+c=sympy.symbols("c")
+#求解空缆线形方程所用的平衡方程
+#联立P58 3-2-16~3-2-18所得
+def balanceFun(z):
+    c=float(z[0])
+    return [-c*sympy.cosh(0.5*l/c-sympy.asinh(0.5*h/(c*sympy.sinh(0.5*l/c)))-0.5*l/c)+c*sympy.cosh(-sympy.asinh(0.5*h/(c*sympy.sinh(0.5*l/c)))-0.5*l/c)-(h/2+n)]
+
+x = sympy.symbols("x")
 #成桥状态线形方程
 y=(h/l)*x+(4*f/l**2)*x*(l-x)    #p57 3-2-11
 
@@ -34,77 +47,43 @@ print('主缆有应力索长S:'+str(S))
 #S=(l/2)*(1+16*n**2)**0.5+0.125*(l/n)*math.log(4*n+(1+16*n**2)**0.5);
 #S=l*(1+8*n**2/3-32*n**4/5);     #有应力索长估算公式（按级数展开结果）以过溪桥为例，误差越1
 
-
-
-c=sympy.symbols("c")
-#s =sympy.solve(-c*sympy.cosh(0.5*l/c-sympy.asinh(0.5*h/(c*sympy.sinh(0.5*l/c)))-0.5*l/c)+c*sympy.cosh(-sympy.asinh(0.5*h/(c*sympy.sinh(0.5*l/c)))-0.5*l/c)-(h/2+n), c)
-#print(float(s))
-def balanceFun(z):
-    c=float(z[0])
-    return [-c*sympy.cosh(0.5*l/c-sympy.asinh(0.5*h/(c*sympy.sinh(0.5*l/c)))-0.5*l/c)+c*sympy.cosh(-sympy.asinh(0.5*h/(c*sympy.sinh(0.5*l/c)))-0.5*l/c)-(h/2+n)]
-
-result=fsolve(balanceFun,[1])
-logger.info('result:\n'+str(result))
+result=fsolve(balanceFun,[1])    #迭代初值为1
+logger.info('平衡方程求解结果:'+str(result))
 
 uy=-result[0]*sympy.cosh(x/result[0]-sympy.asinh(0.5*h/(result[0]*sympy.sinh(0.5*l/result[0])))-0.5*l/result[0])+result[0]*sympy.cosh(-sympy.asinh(0.5*h/(result[0]*sympy.sinh(0.5*l/result[0])))-0.5*l/result[0])   #无应力索线形unstressed y
-logger.info('uy:\n'+str(uy))
-#def balanceFun(z):
-#    c=float(z[0])
-#    c1=float(z[1])
-#    c2=float(z[2])
-#    return [-c*math.cosh((70/2)/c+c1)+c2-(0/2+9/70),
-#            c*math.cosh(c1)-c2,
-#            -math.asinh(0*(2*c*math.sinh(0.5*70/c))**-1)-0.5*70/c-c1]
-    #return [-c*math.cosh((l/2)/c+c1)+c2-(h/2+n),
-    #        c*math.cosh(c1)-c2,
-    #        -math.asinh(h*(2*c*math.sinh(0.5*l/c))**-1)-0.5*l/c-c1]
-#            -math.asinh(h*(2*c*math.sinh(0.5*l/c))**-1)-0.5*l/c-c1]
-#    return [-c*math.cosh((l/2)/c+c1)+c2-(h/2+n),
-#result=fsolve(balanceFun,[7.17,24677,504.246])
-#logger.info('result:\n'+str(result))
+logger.info('空缆状态下的线形方程uy:\n'+str(uy))
 
 #空缆状态下主缆有应力索长
-Ec=2.0*10**11;   #主缆弹性模量(国际单位）
-Ac=0.25*3.14*0.10695**2;   #主缆面积（国际单位）
 
-gb=168000;   #加劲梁自重
+#P61 3-2-34
+#感觉书上的公式有误，3-2-31代入3-2-34，好像结果不对
+#以下2式仅适用于h=0的结果
+#deltaS1=(H*l)*(1+(16*n**2/3))/(Ec*Ac)
+#S1=S-deltaS1
+#logger.info('S1:'+str(S1))
 
-H=gb*l**2/(8*f);
-
-deltaS1=(H*l)*(1+(16*n**2/3))/(Ec*Ac);
-S1=S-deltaS1;
-
-logger.info('S1:'+str(S1))
-
-deltaS11=H*S/(Ec*Ac);
-S11=S-deltaS11;
-logger.info('S11:'+str(S11))
+deltaS1=H*S/(Ec*Ac)
+print('主缆在加劲梁自重作用下的弹性伸长值deltaS1:'+str(deltaS1))
+S1=S-deltaS1
+print('空缆状态下的缆长S1:'+str(S1))
 
 #无应力索长
-gc=8.348*10**4;   #缆索自重
-c=l**2/(8*f);
-logger.info(str(c))
-c=69.5058;
-Hc=c*gc;    #缆索在自重状态下的水平拉力
 
-deltaS2=Hc*(l+c*math.sinh(l/c))/(2*Ec*Ac);
-
-#def fff(x):
-#    return (1-x**2)**(1/2)
-
-#w, err = integrate.quad(f,-1,1)
-#print(w,err)
+#适用于h=0情况
+#c=l**2/(8*f);
+#logger.info(str(c))
+#c=69.5058;
+#Hc=c*gc;    #缆索在自重状态下的水平拉力
+#deltaS2=Hc*(l+c*math.sinh(l/c))/(2*Ec*Ac);
 
 #float(sympy.integrate((1+sympy.diff(y,x)**2)**0.5, (x, 0, l)))
 #float(sympy.integrate((1+sympy.diff(uy,x)**2)**0.5, (x, 0, l)))
-logger.info('deltaS2:'+str(deltaS2))
-deltaS22=Hc*float(sympy.integrate(1+(sympy.diff(uy,x))**2, (x, 0, l)))/(Ec*Ac)
-logger.info('deltaS22:'+str(deltaS22))
+#logger.info('deltaS2:'+str(deltaS2))
 
-from sympy import  integrate ,cos,sin
-from sympy.abc import  a,x,y
-r=integrate(sin(x)/x,(x,-float("inf"),float("inf")))
+#P62 3-2-36
+Hc=result[0]*gc
+deltaS2=Hc*float(sympy.integrate(1+(sympy.diff(uy,x))**2, (x, 0, l)))/(Ec*Ac)
+print('缆索自重作用下的钢缆弹性伸长值deltaS2:'+str(deltaS2))
 
-r1=integrate(sympy.cosh(x))
-logger.info('r1:'+str(r1))
 S0=S-deltaS1-deltaS2;
+print('主缆无应力索长S0:'+str(S0))
